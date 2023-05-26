182723f88527b60f6c1bd3c98f42af175a36b8d3
Your Name <you@example.com>
2023-05-26 16:56:22 -0400

    added 5 day forecast

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ var cityForm = document.getElementById("city-form")
 function getLatLon(city){
     var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid="+ APIKey
     fetch(geoURL).then(function(response){
-        console.log(response)
-        return response.json()
+        // if (!response.ok) {
+        //     alert ("city name not valid");
+        //     return;
+        // } else {
+        //      return response.json()
+        // }
+       return response.json()
     }) .then(function(data){
         console.log(data)
         getWeather(data[0].lat,data[0].lon)
@@ -18,9 +23,15 @@ function getWeather(lat,lon){
         return response.json()
     }).then(function (data) {
         console.log(data)
+        var img = $("<img>").attr("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
         var card = $("<div>").addClass("card")
-        var cardTitle = $("<h2>").addClass("card-title").text(data.name)
-        $("#current").append(card.append(cardTitle))
+        var cardTitle = $("<h2>").addClass("card-title").text(data.name);
+        var cardBody = $("<div>").addClass("card-body");
+        var listGroup = $("<ul>").addClass("list-group");
+        var temp = $("<li>").addClass("list-group-item").text("Temperature: " + Math.round(data.main.temp) + String.fromCharCode(176));
+        var humidity = $("<li>").addClass("list-group-item").text("Humidity: " + Math.round(data.main.humidity) + "%");
+        var wind = $("<li>").addClass("list-group-item").text("Wind: " + Math.round(data.wind.speed) + "mph");
+        $("#current").append(card.append(cardTitle.append(img), cardBody.append(listGroup.append(temp, humidity, wind))))
     }) 
     getForecast(lat,lon)
 }
@@ -40,6 +51,21 @@ function getForecast(lat, lon) {
             }
         }
        console.log(forecastArray) 
+       for (var i=0; i<forecastArray.length; i++) {
+        var data = forecastArray[i]
+        console.log(data)
+        var dateDisplay = dayjs.unix(data.dt).format("dddd")
+        console.log(dateDisplay)
+           var img = $("<img>").attr("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+           var card = $("<div>").addClass("card")
+           var cardTitle = $("<h2>").addClass("card-title").text(dateDisplay);
+           var cardBody = $("<div>").addClass("card-body");
+           var listGroup = $("<ul>").addClass("list-group");
+           var temp = $("<li>").addClass("list-group-item").text("Temperature: " + Math.round(data.main.temp) + String.fromCharCode(176));
+           var humidity = $("<li>").addClass("list-group-item").text("Humidity: " + Math.round(data.main.humidity) + "%");
+           var wind = $("<li>").addClass("list-group-item").text("Wind: " + Math.round(data.wind.speed) + "mph");
+           $("#forecast").append(card.append(cardTitle.append(img), cardBody.append(listGroup.append(temp, humidity, wind))))
+       }
     })
 }
 //Add click to search button
